bcb9a910d9c19f3e3a575f6c56ddb500950b6ec4
KozekiUii <2389895003@qq.com>
2026-07-03 10:55:10 +0800

    feat: add minimal loadable kernel module

---
+++ module/hellomodule/hellomodule.c
@@ -0,0 +1,30 @@
+/* ************************************************************************
+> File Name:     hellomodule.c
+> Author:        程序员Carl
+> 微信公众号:    代码随想录
+> Created Time:  2026年07月03日 星期五 09时23分40秒
+> Description:   
+ ************************************************************************/
+#include <linux/module.h>
+#include<linux/init.h>
+#include<linux/kernel.h>
+
+static int __init hello_init(void)
+{
+        printk(KERN_EMERG "{KERN_EMERG} Hello Module Init!\n");
+        printk("{default} Hello Module Init!\n");
+        return 0;
+}
+
+static void __exit hello_exit(void)
+{
+    printk("{default} Hello Module Exit!\n");
+}
+
+module_init(hello_init);
+module_exit(hello_exit);
+
+MODULE_LICENSE("GPL2");
+MODULE_AUTHOR("Komorebi");
+MODULE_DESCRIPTION("hello module");
+MODULE_ALIAS("test_module");
